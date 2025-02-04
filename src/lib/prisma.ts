84f1a5d2db6b99de58477a8a4b8e85682b5cbc29
Gia bao <giabaod345@gmail.com>
2025-02-04 11:52:35 +0700

    production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
-// Dùng biến toàn cục để tránh tạo nhiều instance của Prisma
-const prisma = global.prisma ?? new PrismaClient();
-
-// Nếu đang ở môi trường phát triển, lưu Prisma client vào `global` để tránh tạo nhiều kết nối
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
-
-export { prisma };
+export default prisma;
